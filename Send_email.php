dbf7f01f4de2dd347da881be179612f0aa3e2d1e
hiroputra <hiroputra@gmail.com>
2020-03-12 11:13:01 +0700

    Add files via upload

---
+++ Send_email.php
@@ -0,0 +1,74 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Send_email extends CI_Controller {
+
+    /**
+     * Kirim email dengan SMTP Gmail.
+     *
+     */
+    public function index()
+    {
+        $this->load->library('email');
+
+            // $config['protocol']    = 'smtp';
+            // $config['smtp_host']    = 'ssl://smtp.googlemail.com';
+            // $config['smtp_port']    = '465';
+            // $config['smtp_timeout'] = '7';
+            // $config['smtp_user']    = 'helpdesk.nam19@gmail.com';
+            // $config['smtp_pass']    = 'Narada1234!@#$';
+            // $config['charset']    = 'utf-8';
+            // $config['newline']    = "\r\n";
+            // $config['wordwrap']     = TRUE;
+            // $config['mailtype'] = 'text'; // or html
+            // $config['validation'] = FALSE;
+
+            $config['protocol']    = 'smtp';
+            $config['smtp_host']    = 'ssl://mail.gijagijug.com';
+            $config['smtp_port']    = '465';
+            $config['smtp_timeout'] = '7';
+            $config['smtp_user']    = 'hiro@gijagijug.com';
+            $config['smtp_pass']    = 'sysnki1919';
+            $config['charset']    = 'utf-8';
+            $config['newline']    = "\r\n";
+            $config['wordwrap']     = TRUE;
+            $config['mailtype'] = 'text'; // or html
+            $config['validation'] = FALSE;
+
+
+            $this->email->initialize($config);
+
+        // Load library email dan konfigurasinya
+      
+      $this->email->initialize($config);
+        // Email dan nama pengirim
+      $this->email->from('hiro@gijagijug.com', 'hiro@gijagijug.com');
+
+        // Email penerima
+        $this->email->to('hiroputra@gmail.com'); // Ganti dengan email tujuan
+
+        // Lampiran email, isi dengan url/path file
+        //$this->email->attach('https://masrud.com/content/images/20181215150137-codeigniter-smtp-gmail.png');
+
+        // Subject email
+        $this->email->subject('Kirim Email dengan SMTP Gmail CodeIgniter | MasRud.com');
+
+        // Isi email
+        $this->email->message("tes");
+
+        // Tampilkan pesan sukses atau error
+        // if ($this->email->send()) {
+        //     echo 'Sukses! email berhasil dikirim.';
+
+        // } else {
+        //     echo $this->email->print_debugger(array('headers'));
+        //     //echo 'Error! email tidak dapat dikirim.';
+        // }
+
+        if ($this->email->send()) {
+            echo 'Sukses! email berhasil dikirim.';
+        } else {
+            show_error($this->email->print_debugger());
+        }
+    }
+}
---
+++ Sendingemail_Controller.php
@@ -0,0 +1,65 @@
+<?php
+class Sendingemail_Controller extends CI_Controller {
+    function __construct() {
+        parent::__construct();
+        $this->load->library('session');
+        $this->load->helper('form');
+    }
+    public function index() {
+        $this->load->helper('form');
+        $this->load->view('contact_email_form');
+    }
+    public function send_mail() {
+        $from_email = 'hiro@gijagijug.com';
+        $to_email = $this->input->post('email');
+        //Load email library
+		        $config = array(
+        'protocol'  => 'smtp',
+        'smtp_host' => 'smtp.gmail.com',
+        'smtp_port' => 465,
+        'smtp_user' => 'helpdesk.nam19@gmail.com',
+        'smtp_pass' => 'Narada1234!@#$',
+        'mailtype'  => 'html',
+        'charset'   => 'utf-8'
+        )
+		
+        $this->load->library('email'$config);
+
+        $config = array(
+        'protocol'  => 'smtp',
+        'smtp_host' => 'smtp.gmail.com',
+        'smtp_port' => 465,
+        'smtp_user' => 'helpdesk.nam19@gmail.com',
+        'smtp_pass' => 'Narada1234!@#$',
+        'mailtype'  => 'html',
+        'charset'   => 'utf-8'
+        );
+		        $this->load->library("email", $config);
+        
+        if ($file_path!=''){
+        $this->email->attach($file_path);
+        }
+        
+        $this->email->set_newline("\r\n");
+        $this->email->from($from_email, $from_email);
+        $this->email->to($to_email);
+        $this->email->cc($cc_email);
+        $this->email->bcc($bcc_email);
+        $this->email->subject($subject);
+        $this->email->set_mailtype('html');
+        $this->email->initialize($config);
+
+
+        $this->email->from($from_email, 'Identification');
+        $this->email->to($to_email);
+        $this->email->subject('Send Email Codeigniter');
+        $this->email->message('The email send using codeigniter library');
+        //Send mail
+        if($this->email->send())
+        $this->session->set_flashdata("email_sent","Congragulation Email Send Successfully.");
+        else
+        $this->session->set_flashdata("email_sent","You have encountered an error");
+        $this->load->view('contact_email_form');
+    }
+}
+?>
